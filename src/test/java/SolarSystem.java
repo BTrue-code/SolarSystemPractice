22b417ffa62cf045717841de52321ebe631cff31
btrue-code <b.true1990@gmail.com>
2025-06-02 20:11:42 -0400

    solar system basic structure is ready

---
+++ src/test/java/SolarSystem.java
@@ -0,0 +1,15 @@
+public class SolarSystem {
+
+    public static void main(String[] args) {
+
+        BasicStructure bb = new BasicStructure();
+
+        bb.Planets = "2";
+        bb.Moon = "1";
+        bb.Stars = "10";
+        bb.Sun = "1";
+
+        System.out.println("solar system is ready");
+    }
+
+}
